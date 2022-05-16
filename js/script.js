4eda77114ddccf380b236416c97787f335f5ed90
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-05-16 07:01:38 -0400

    Currently working on the error messages for the form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-function validateForm() {
-  var x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
+let forms = document.querySelector('.form-container')
+let inputFields = document.querySelector('.fields input')
+
+// Adding an event listener on form submit to prevent the form from submitting.
+
+forms.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Iterates through each input and adds an event listener of submit on each.
+
+  inputFields.forEach((input) => {
+    input.addEventListener("submit", () => {
+      // If the input does not have a value then add the error message.
+      if (!input.value) {
+        document.classList.add("error");
+      }
+
+      // If the input does not have a value then remove the error message.
+      document.classList.remove("error");
+    });
+  });
+
+})
